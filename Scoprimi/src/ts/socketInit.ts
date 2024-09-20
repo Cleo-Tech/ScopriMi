e0c6eed38ba6ab95adeed1673e92ce1e1d7f8b0a
pesto13 <ricola1304@gmail.com>
2024-09-21 01:41:05 +0200

    fix nuovi link

--- Scoprimi/src/ts/socketInit.ts
+++ Scoprimi/src/ts/socketInit.ts
@@ -1,7 +1,7 @@
 import { io } from 'socket.io-client';
 
 const webServerBaseUrl = process.env.NODE_ENV === 'production'
-  ? 'https://party-game-backendeu.onrender.com'
+  ? 'https://backed-scoprimi.onrender.com'
   : 'http://localhost:3001';
 
 const socket = io(webServerBaseUrl, {
