752a40adb1ab4cacd85e8f4082a9b16ca106623f
pesto13 <ricola1304@gmail.com>
2024-10-09 22:45:15 +0200

    risolto podio

--- Scoprimi/src/components/finalresults/FinalResults.tsx
+++ Scoprimi/src/components/finalresults/FinalResults.tsx
@@ -2,6 +2,36 @@ import React from 'react';
 import { FinalResultData } from '../../ts/types';
 import { useLocation, useNavigate } from 'react-router-dom';
 
+const defaultHeights = [
+  '7vh',
+  '8vh',
+  '6vh',
+];
+
+const defaultColors = [
+  '#8a9597', // Argento
+  '#cda434', // Oro
+  '#cd7f32', // Bronzo
+];
+
+function getPodiumStats(sameScore1And2, sameScore1And3, sameScore2And3, positionsArray) {
+  let podiumArray = [...positionsArray];
+
+  if (sameScore1And2 && sameScore1And3 && sameScore2And3) {
+    podiumArray = [positionsArray[1], positionsArray[1], positionsArray[1]]; // Tutti oro
+  }
+  else if (!sameScore1And2 && sameScore2And3) {
+    podiumArray = [positionsArray[0], positionsArray[1], positionsArray[0]]; // 1 oro 2 argento
+  }
+  else if (sameScore1And2 && !sameScore2And3) {
+    podiumArray = [positionsArray[1], positionsArray[1], positionsArray[2]]; // 2 oro 1 bronzo
+  }
+  else {
+    podiumArray = [...positionsArray]; // Mantieni l'ordine normale
+  }
+
+  return podiumArray;
+}
 const FinalResults: React.FC = () => {
   const location = useLocation();
   const navigate = useNavigate();
@@ -28,16 +58,13 @@ const FinalResults: React.FC = () => {
   let sameScore1And3 = false;
 
   if (podium.length >= 2) {
-    sameScore1And2 = podium[0][1].score === podium[1][1].score;
-    sameScore2And3 = podium[1][1].score === podium[2]?.[1]?.score;
-    sameScore1And3 = podium[0][1].score === podium[2]?.[1]?.score;
+    sameScore1And2 = podium[1][1].score === podium[0][1].score;
+    sameScore2And3 = podium[0][1].score === podium[2]?.[1]?.score;
+    sameScore1And3 = podium[1][1].score === podium[2]?.[1]?.score;
   }
 
-  console.log('sameScore1And2:', sameScore1And2);
-  console.log('sameScore2And3:', sameScore2And3);
-  console.log('sameScore1And3:', sameScore1And3);
-
-  const allScoresEqual = sameScore1And2 && sameScore2And3;
+  const actualHeight = getPodiumStats(sameScore1And2, sameScore1And3, sameScore2And3, defaultHeights);
+  const actualColors = getPodiumStats(sameScore1And2, sameScore1And3, sameScore2And3, defaultColors);
 
   return (
     <>
@@ -46,38 +73,9 @@ const FinalResults: React.FC = () => {
 
         <div className='podium' style={{ display: 'flex', justifyContent: 'space-between', marginTop: '10px', width: '100%' }}>
           {podium.map(([player, { score, image }], index) => {
-            let heightStyle;
-            if (allScoresEqual) {
-              heightStyle = { height: '7vh' };
-            } else if (sameScore1And2 && index <= 1) {
-              heightStyle = { height: '7vh' };
-            } else if (sameScore2And3 && index >= 1) {
-              heightStyle = { height: '6vh' };
-            } else if (sameScore1And2 && sameScore1And3 && index !== 1) {
-              heightStyle = { height: '7vh' };
-            } else {
-              heightStyle = index === 0 ? { height: '7vh' } : index === 1 ? { height: '8vh' } : { height: '6vh' };
-            }
 
-            let backgroundColor;
-
-            // Assegna i colori in base alle condizioni
-            if (allScoresEqual) {
-              backgroundColor = '#cda434'; // Tutti oro
-            } else if (sameScore1And2 && !sameScore2And3) {
-              backgroundColor = index <= 1 ? '#cda434' : '#cd7f32'; // Primi due oro, terzo bronzo
-            } else if (sameScore2And3 && !sameScore1And2) {
-              // Se secondo e terzo hanno lo stesso punteggio, entrambi argento
-              backgroundColor = index === 0 ? '#cda434' : '#8a9597'; // Primo oro, secondo e terzo argento
-            } else {
-              // Altrimenti, applica i colori predefiniti
-              const backgroundColors = {
-                1: '#cda434', // Oro
-                0: '#8a9597', // Argento
-                2: '#cd7f32', // Bronzo
-              };
-              backgroundColor = backgroundColors[index];
-            }
+            const heightStyle = actualHeight[index];
+            const backgroundColor = actualColors[index];
 
             return (
               <div
@@ -86,7 +84,7 @@ const FinalResults: React.FC = () => {
                 style={{ display: 'flex', justifyContent: 'flex-end', flexDirection: 'column', alignItems: 'center', flex: '1 1 0%', margin: '0 10px' }}
               >
                 <img src={image} alt={`${player} avatar`} style={{ height: '70px' }} />
-                <div style={{ ...heightStyle, backgroundColor, display: 'flex', justifyContent: 'center', alignItems: 'center', width: '100%', borderRadius: '10px' }}>
+                <div style={{ height: heightStyle, backgroundColor, display: 'flex', justifyContent: 'center', alignItems: 'center', width: '100%', borderRadius: '10px' }}>
                   <p style={{ margin: '0' }}>{score}</p>
                 </div>
                 <p style={{ textAlign: 'center' }}>{player}</p>
