925087e73cb028b555783f873195d123b20a9b32
SIMONE MUSSINI <284900@studenti.unimore.it>
2024-10-11 09:44:35 +0200

    MHANZ

--- Scoprimi/src/components/game/Game.tsx
+++ Scoprimi/src/components/game/Game.tsx
@@ -15,12 +15,14 @@ import { Question, QuestionMode } from '../../../../Server/src/data/Question';
 // Funzione per il parsing di filename di immagini
 export const todoShitFunction = (votestring: string) => {
   const wordlList = votestring.substring(votestring.lastIndexOf('/') + 1).split('_');
+
+  // Gestisce nel caso il filename non sia sburato come li rende cloudinary aggiungendo roba a caso dopo il nome. Ex. vedi il file 'fursuit.jpg'
   if (wordlList.length === 1) {
     return wordlList[0].replace(/\.(.*)$/, '');
-  }   // Gestisce nel caso il filename non sia sburato come li rende cloudinary aggiungendo roba a caso dopo il nome. Ex. vedi il file 'fursuit.jpg'
+  }
   wordlList.pop();
   return wordlList.join(' ');
-}
+};
 
 const Game: React.FC = () => {
   const [question, setQuestion] = useState<string>('');
