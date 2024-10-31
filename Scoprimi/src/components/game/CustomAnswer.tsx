e0cf2478f0a87e192e85001a17ea3f1278f99f0b
pesto13 <ricola1304@gmail.com>
2024-10-31 16:45:21 +0100

    layout sistemato

--- Scoprimi/src/components/game/CustomAnswer.tsx
+++ Scoprimi/src/components/game/CustomAnswer.tsx
@@ -13,30 +13,34 @@ const CustomAnswer: React.FC<CustomAnswerProps> = ({ handleSubmit }) => {
   };
 
   return (
-    <div className='elegant-background' style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', gap: '10px' }}>
-      <label>La tua risposta:</label>
-      <input
-        className="my-input"
-        type="text"
-        onChange={(e) => setAnswer(e.target.value)}
-        style={{
-          color: 'var(--background-color)',
-        }}
-      />
-      <button
-        className="my-btn"
-        onClick={() => {
-          handleSubmit(answer);
-          handleClick();
-        }}
-        style={{
-          backgroundColor: btnClicked ? 'var(--disabled-color)' : 'var(--success-color)',
-        }}
-        disabled={btnClicked ? true : false} // Gestisce il problema del voto multiplo, ma resta da fixare nel backend!!
-      >
-        Conferma
-      </button>
-    </div>
+    // <div className='fill' style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+    <div className='fill'>
+      <div className='elegant-background' style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', gap: '10px', marginTop: '15vh' }}>
+        <label>La tua risposta:</label>
+        <input
+          className="my-input"
+          type="text"
+          onChange={(e) => setAnswer(e.target.value)}
+          style={{
+            color: 'var(--background-color)',
+            width: '90%',
+          }}
+        />
+        <button
+          className="my-btn mt-3"
+          onClick={() => {
+            handleSubmit(answer);
+            handleClick();
+          }}
+          style={{
+            backgroundColor: btnClicked ? 'var(--disabled-color)' : 'var(--success-color)',
+          }}
+          disabled={btnClicked ? true : false} // Gestisce il problema del voto multiplo, ma resta da fixare nel backend!!
+        >
+          Conferma
+        </button>
+      </div>
+    </div >
   );
 };
 
