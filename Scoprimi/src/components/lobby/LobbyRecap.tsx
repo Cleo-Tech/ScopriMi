4adef114d3889cce0969e8e831c3e1b615466b67
pesto13 <ricola1304@gmail.com>
2024-11-03 17:43:29 +0100

    smash LobbyRecap

---
+++ Scoprimi/src/components/lobby/LobbyRecap.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import { Game } from '../../../../Server/src/data/Game.ts';
+
+interface LobbyRecapProps {
+  lobby: Game;
+  onModify: (lobbyCode: string) => void;
+}
+
+const LobbyRecap: React.FC<LobbyRecapProps> = ({ lobby, onModify }) => {
+  const gearIconStyle: React.CSSProperties = {
+    fontSize: '24px', // Dimensione dell'icona
+    color: '#fff', // Colore dell'icona
+    padding: '10px', // Padding per aumentare l'area cliccabile
+    border: '2px solid #444', // Bordo scuro
+    borderRadius: '16px', // Rende il bordo arrotondato
+    backgroundColor: '#333', // Sfondo scuro per evidenziare
+    cursor: 'pointer', // Cambia il cursore quando si passa sopra l'icona
+    transition: 'background-color 0.3s', // Transizione per l'effetto hover
+  };
+
+  return (
+    <tr
+      onClick={() => !lobby.isGameStarted && onModify(lobby.lobbyCode)}
+      className={`lobby-row ${lobby.isGameStarted ? 'disabled' : ''}`}
+    >
+      <td>{Object.keys(lobby.players).length}</td>
+      <td>{lobby.lobbyCode}</td>
+      <td>
+        <i
+          className="my-i fa-solid fa-gear"
+          style={gearIconStyle}
+          onClick={(e) => {
+            e.stopPropagation(); // Previene l'attivazione del click sulla riga
+            onModify(lobby.lobbyCode);
+          }}
+        ></i>
+      </td>
+    </tr>
+  );
+};
+
+export default LobbyRecap;
